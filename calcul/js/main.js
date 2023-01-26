71b8e40f76e638f7893654284bb742c01a91da2b
benbarFatimaEzzahra <fatimaezzahra.benbar2003@gmail.com>
2023-01-26 16:15:19 +0100

    first commit

---
+++ calcul/js/main.js
@@ -0,0 +1,16 @@
+function calculer() 
+{ 
+    let a = document.getElementById("output").value 
+    let b = eval(a) 
+    document.getElementById("output").value = b 
+} 
+//fonction qui affiche la valeur
+function afficher(val) 
+{ 
+    document.getElementById("output").value+=val 
+} 
+//fonction qui efface l'écran 
+function effacer() 
+{ 
+    document.getElementById("output").value = "" 
+} 
